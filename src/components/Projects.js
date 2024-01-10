627fbb9fa37fcf0964ecf5a5fc11c6720537a79a
kajal-2011 <kajalrekha888@gmail.com>
2024-01-11 00:08:42 +0600

    project update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,6 +25,32 @@ const data = [
   },
   {
     id: 2,
+    title: "Luminex - An Online Jewlery Shop",
+    img1: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1704908056/Screenshot_69_gczjrg.png",
+    img2: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1704908207/Screenshot_71_ctmelv.png",
+    description:
+      "Luminex is an online jewelry shop. Seamlessly navigate through stunning product details, secure user authentication, and a smooth shopping cart. Powered by Next.js, TypeScript, Redux, Redux Persist, SwiperJS, SWR, CLSX, Lenix, and more.",
+    tools: [
+      "Next.js",
+      "TypeScript",
+      "Redux",
+      "Pedux Persist",
+      "Axios",
+      "Mongodb",
+      "Tailwind CSS",
+      "Swiper.js",
+      "SWR",
+      "Clsx",
+      "Lenis",
+      "Framer Motion",
+      "Aos",
+    ],
+    liveLink: "https://luminex-lux.vercel.app",
+    frontEndLink: "https://github.com/kajal-rekha/luminex-client.git",
+    backendLink: "https://github.com/kajal-rekha/luminex-server.git",
+  },
+  {
+    id: 3,
     title: "Proxima - A Powerful Project Management App",
     img1: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1677934511/Screenshot_33_b3fq76.png",
     img2: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1677934525/Screenshot_35_f60au2.png",
@@ -44,7 +70,7 @@ const data = [
     backendLink: "https://github.com/kajal-rekha/proxima-server.git",
   },
   {
-    id: 3,
+    id: 4,
     title: "Tech Alph _ An Ecommerce Website",
     img1: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1677934825/Screenshot_36_efvmrv.png",
     img2: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1675278769/Screenshot_2_xgqpc2.png",
@@ -63,7 +89,7 @@ const data = [
     backendLink: "https://github.com/kajal-rekha/tech-alpha-server.git",
   },
   {
-    id: 4,
+    id: 5,
     title: "Foodvarse _ A Recipe Finder Web Application",
     img1: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1675278752/Screenshot_4_rfupr3.png",
     img2: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1675278735/Screenshot_5_tkwlqi.png",
@@ -74,7 +100,7 @@ const data = [
     frontEndLink: "https://github.com/kajal-rekha/foodvarse.git",
   },
   {
-    id: 5,
+    id: 6,
     title: "React Todo App _ A simple Todo application",
     img1: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1692877869/Screenshot_8_gxekew.png",
     img2: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1699009093/kajal-todo-app.netlify.app__eamdve.png",
@@ -86,7 +112,7 @@ const data = [
     backendLink: "https://github.com/kajal-rekha/todo-app-server.git",
   },
   {
-    id: 6,
+    id: 7,
     title: "Immemorial: 90's Memory",
     img1: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1692878024/Screenshot_9_fpzwdb.png",
     img2: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1699009698/Screenshot_11_swjnwj.png",
@@ -100,9 +126,9 @@ const data = [
 
 const Projects = () => {
   return (
-    <div className="projects container mx-auto mt-40" id="projects">
+    <div className='projects container mx-auto mt-40' id='projects'>
       <SectionTitle title={"Projects"} />
-      <div className="projects-wrapper mt-40 flex flex-col gap-40">
+      <div className='projects-wrapper mt-40 flex flex-col gap-40'>
         {data.map((project) => (
           <Project key={project.id} project={project} />
         ))}
